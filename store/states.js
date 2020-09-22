14eaf33eceb180b2770682c7762a370fbe2698ff
maekawa <idler0801@gmail.com>
2020-09-22 15:29:57 +0900

    initial data to store

--- store/states.js
+++ store/states.js
@@ -1,9 +1,9 @@
 export const state = () => ({
-  data: [],
+  data: ["initial"],
 })
 export const mutations = {
   async setData (state, res) {
-    state.data = res;
+    state.data.push(...res);
   },
 }
 export const actions = {
